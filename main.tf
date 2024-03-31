a8fe058b3b1b080d5532bc2c1ed54d2408c964e6
29usama <muhammadusama2117@gmail.com>
2024-03-31 15:39:39 +0500

    altering files

--- main.tf
+++ main.tf
@@ -1,10 +1,16 @@
 provider "google" {
-  credentials = jsondecode(base64decode(var.GOOGLE_CREDENTIALS))
-  project     = "gmzdev"
-  region      = "us-central1"
+  project = "gmzdev"
+  region  = "us-central1"
 }
 
 resource "google_compute_network" "vpc_network" {
-  name                    = "vpc-githubActions"
+  name                    = "my-vpc"
   auto_create_subnetworks = false
+}
+
+resource "google_compute_subnetwork" "vpc_subnet" {
+  name          = "my-subnet"
+  ip_cidr_range = "10.0.0.0/24"
+  network       = google_compute_network.vpc_network.self_link
+  region        = "us-central1"
 }
