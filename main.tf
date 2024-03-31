508a24d080c2eabe35161114601d3579307ad178
29usama <muhammadusama2117@gmail.com>
2024-03-31 14:39:41 +0500

    adding latest json file

--- main.tf
+++ main.tf
@@ -8,7 +8,7 @@
 #}
 
 provider "google" {
-  credentials = file("/home/usama/Downloads/gmzdev-450117b05ad8.json")
+  credentials = file("/home/usama/Downloads/gmzdev-f3ac397c9141.json")
   project     = "gmzdev"
   region      = "us-central1"
 }
