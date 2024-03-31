e45bbb996347d800cda48e836529d6c8c0a03b14
29usama <muhammadusama2117@gmail.com>
2024-03-31 15:13:42 +0500

    push the latest code

--- main.tf
+++ main.tf
@@ -1,20 +1,10 @@
-#terraform {
-#  required_providers {
-#    google = {
-#      source = "hashicorp/google"
-#      version = "5.22.0"
-#    }
-#  }
-#}
-
 provider "google" {
-  credentials = file("/home/usama/Downloads/personal_gcp_key/gmzdev-f3ac397c9141.json")
+  credentials = jsondecode(base64decode(var.GOOGLE_CREDENTIALS))
   project     = "gmzdev"
   region      = "us-central1"
 }
 
 resource "google_compute_network" "vpc_network" {
   name                    = "vpc-githubActions"
-  auto_create_subnetworks = "false"
+  auto_create_subnetworks = false
 }
-
